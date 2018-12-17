38fed52fdae12727855f5d20e4d792b51e17caf0
Jeremy Robertson <sparticus7n7@yahoo.com>
2018-12-17 09:36:41 -0700

    removing react-redux

--- src/App.js
+++ src/App.js
@@ -2,76 +2,80 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//NEW IMPORTS 
+//NEW IMPORTS
 // To make this component work we need to 'connect' it with our top level redux store
-import { connect } from 'react-redux'
-import { updateName, addPerson } from './reducer'
+import store from './store';
+import { updateName, addPerson } from './reducer';
 
 class App extends Component {
+  componentDidMount() {
+    let newState;
+    const unsubscribe = store.subscribe(appState => {
+      newState = {
+        name: appState.name,
+        people: appState.people,
+      };
+      this.setState(newState);
+    });
+    this.setState({
+      unsubscribe,
+    });
+  }
+  componentWillUnmount() {
+    if (this.state.unsubscribe) {
+      this.state.unsubscribe();
+    }
+  }
+  updateName(newName) {
+    const action = updateName(newName);
+    store.dispatch(action);
+  }
+  addPerson(age, name) {
+    const action = addPerson(age, name);
+    store.dispatch(action);
+  }
   render() {
-    let { name, people } = this.props; //We can type this.props a lot less if we do this.
+    let { name, people } = this.state; //We can type this.props a lot less if we do this.
 
     //We got people from props, which came from mapStateToProps, which came from Redux
-    let peopleRender = people.map((person) => (
+    let peopleRender = people.map(person => (
       <div>
         <strong>{person.name}</strong> - <span>{person.age}</span>
       </div>
-    ))
+    ));
 
     return (
       <div className="App">
         <p>
           <h4>{name}</h4>
-          <input type="text" ref="name"/>
-          <button onClick={()=> this.props.updateName(this.refs.name.value)}>Update</button>    
+          <input type="text" ref="name" />
+          <button onClick={() => this.updateName(this.refs.name.value)}>
+            Update
+          </button>
         </p>
         <p>
           <h4>New Person</h4>
           <div>
             <h5>Name</h5>
-            <input ref="newName"></input>
+            <input ref="newName" />
             <h5>Age</h5>
-            <input ref="newAge"></input>
-            <button onClick={()=> this.props.addPerson(this.refs.newAge.value, this.refs.newName.value)}>add</button>
+            <input ref="newAge" />
+            <button
+              onClick={() =>
+                this.addPerson(this.refs.newAge.value, this.refs.newName.value)
+              }
+            >
+              add
+            </button>
           </div>
         </p>
-        <p>
-          {peopleRender}
-        </p>
+        <p>{peopleRender}</p>
       </div>
     );
   }
 }
 
-//Input function to get data from state
-function moveFromStoreToProps(state) {
-  //state refers to the redux state
-  if(!state) return {};
-
-  let {people, name} = state;
-
-  // return { //This object gets mashed/merged into this.props
-  //   people,
-  //   name
-  // }
-  return { //This object gets mashed/merged into this.props
-    people: people,
-    name: name
-  }
-}
-
-//Output object with all of the actions that this component will update our store about
-
-// We need to import any actions we want to send through the process
-// We importeded these up above with this line of code :
-// import {updateAge, addPerson} from './reducer'
-let outputActions = {
-  updateName: updateName,
-  addPerson: addPerson
-}
-
-let reduxInsAndOuts = connect(moveFromStoreToProps, outputActions)
-export default reduxInsAndOuts(App);
+export default App;
 //This is often done in a single line of code like this:
 //connect(mapStateToProps, outputActions)(App)
 
--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-//NEW IMPORTS HERE
-import store from './store'
-import { Provider } from 'react-redux' //Notice react-redux, not redux. 
-
-//Index is very commonly used for setup.  We don't do routes, or other actual code in here.  Just wire up pieces.
-
 //Putting the provider here gives it top level access to the whole app so it can tell React to render when needed.
-let topLevelComponentWithStore = ( 
-    <Provider store={store}>
-        <App />
-</Provider>)
-
-ReactDOM.render(topLevelComponentWithStore, document.getElementById('root'));
+let topLevelComponentWithStore = (<App />,
+ReactDOM.render(topLevelComponentWithStore, document.getElementById('root')));
 registerServiceWorker();
